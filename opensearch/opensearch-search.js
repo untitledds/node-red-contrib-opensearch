d7b2f9ed8bcf31e4bc3d11a1ea1ed3a9a30eca99
Aleksey Dymchenko <untitledds@ya.ru>
2025-04-24 14:12:20 +0300

    payload done

--- opensearch/opensearch-search.js
+++ opensearch/opensearch-search.js
@@ -1,159 +1,142 @@
 module.exports = function (RED) {
-  "use strict";
+  'use strict';
 
-  function opensearchSearchNode(config) {
-    try {
-      const node = this;
-      RED.nodes.createNode(node, config);
+  function OpenSearchSearchNode(config) {
+    RED.nodes.createNode(this, config);
+    const node = this;
+    const serverConfig = RED.nodes.getNode(config.server);
 
-      // Получаем конфигурацию сервера OpenSearch
-      const serverConfig = RED.nodes.getNode(config.server);
-      if (!serverConfig || !serverConfig.client) {
-        node.status({
-          fill: "red",
-          shape: "dot",
-          text: "No OpenSearch client found",
-        });
-        return;
-      }
+    if (!serverConfig || !serverConfig.client) {
+      node.status({ fill: 'red', shape: 'dot', text: 'No OpenSearch client' });
+      return;
+    }
 
-      node.status({});
-
-      // Обработка входящих сообщений
-      node.on("input", function (msg) {
+    node.on('input', function (msg, send, done) {
+      try {
+        // Подготовка конфигурации поиска
         const searchConfig = {
-          index: config.index, // Индекс из конфигурации узла
-          body: config.query, // Запрос из конфигурации узла
-          size: config.size, // Количество результатов
-          from: config.from, // Смещение
+          index: msg.index || config.index,
+          body: msg.query || config.query || { query: { match_all: {} } },
+          size: msg.size || config.size || 10,
+          from: msg.from || config.from || 0,
         };
 
-        // Переопределение индекса из входящего сообщения
-        if (msg.index) {
-          searchConfig.index = msg.index;
-        }
-
-        // Переопределение запроса из входящего сообщения
-        if (msg.query) {
-          searchConfig.body = msg.query;
-        }
-
-        // Переопределение размера страницы
-        searchConfig.size = 10; // По умолчанию
-        if (config.size) {
-          searchConfig.size = config.size;
-        }
-        if (msg.size) {
-          searchConfig.size = msg.size;
-        }
-
-        // Переопределение смещения
-        if (msg.from) {
-          searchConfig.from = msg.from;
-        }
-
-        // Поддержка scroll
-        if (config.scroll || msg.scroll) {
-          searchConfig.scroll = "1m"; // Время жизни scroll-курсора
-        }
-
-        // Инициализация payload и es_responses
+        // Инициализация выходных данных
         msg.payload = [];
         if (config.fullResponse) {
           msg.es_responses = [];
         }
 
-        // Выполнение запроса к OpenSearch
-        serverConfig.client.search(searchConfig).then(
-          function (resp) {
-            (function next(resp) {
-              // Добавление полного ответа, если требуется
-              if (config.fullResponse) {
-                msg.es_responses.push(resp);
-              }
-
-              // Проверка наличия результатов
-              if (!resp.hits || !resp.hits.hits || !resp.hits.hits.length) {
-                if (msg.payload.length > 0) {
-                  node.send(msg); // Отправляем оставшиеся данные
-                }
-                return;
-              }
-
-              // Обработка найденных документов
-              if (config.bulkSize) {
-                // Пакетная обработка
-                for (const hit of resp.hits.hits) {
-                  msg.payload.push({
-                    _source: hit._source,
-                    _id: hit._id,
-                  });
-
-                  // Отправка данных пакетами
-                  if (msg.payload.length % config.bulkSize === 0) {
-                    node.send({ ...msg }); // Клонируем msg
-                    msg.payload = [];
-                  }
-                }
-              } else {
-                // Полная загрузка
-                msg.payload = resp.hits.hits.map((hit) => ({
-                  ...hit._source,
-                  _meta: {
-                    id: hit._id,
-                    index: hit._index,
-                    score: hit._score,
-                  },
-                }));
-              }
-
-              // Если scroll не используется, отправляем оставшиеся данные
-              if (!searchConfig.scroll || !resp.hits.hits.length) {
-                if (msg.payload.length > 0) {
-                  node.send(msg); // Отправляем оставшиеся данные
-                }
-                return;
-              }
-
-              // Продолжение scroll-запроса
-              if (searchConfig.scroll) {
-                const scrollId = resp._scroll_id;
-                serverConfig.client
-                  .scroll({
-                    scroll: "1m",
-                    scroll_id: scrollId,
-                  })
-                  .then(next)
-                  .catch((err) => node.error("Scroll error: " + err));
-              }
-            })(resp);
-          },
-          function (err) {
-            node.error("OpenSearch Search Node Error - " + err.message);
-            msg.payload = [];
-            msg.error = err;
-            node.send(msg);
-          }
-        );
-      });
-
-      // Обработка ошибок узла
-      node.on("error", function (error) {
-        node.error("OpenSearch Search Node Error - " + error);
-      });
-
-      // Очистка ресурсов при закрытии узла
-      node.on("close", function (done) {
-        if (node.client) {
-          delete node.client;
+        // Обработка scroll-запросов
+        if (config.scroll || msg.scroll) {
+          searchConfig.scroll = '1m';
+          processScrollSearch();
+        } else {
+          processNormalSearch();
         }
-        done();
-      });
-    } catch (err) {
-      node.error("OpenSearch Search Node Error - " + err);
-    }
+
+        function processNormalSearch() {
+          serverConfig.client
+            .search(searchConfig)
+            .then((resp) => {
+              processResponse(resp);
+              if (done) done();
+            })
+            .catch((err) => handleError(err));
+        }
+
+        function processScrollSearch() {
+          let scrollId = null;
+
+          const processScrollResponse = (resp) => {
+            processResponse(resp);
+            scrollId = resp._scroll_id;
+
+            if (resp.hits.hits.length === 0) {
+              if (done) done();
+              return Promise.resolve();
+            }
+
+            return serverConfig.client
+              .scroll({
+                scroll_id: scrollId,
+                scroll: '1m',
+              })
+              .then(processScrollResponse);
+          };
+
+          serverConfig.client
+            .search(searchConfig)
+            .then(processScrollResponse)
+            .catch((err) => handleError(err));
+        }
+
+        function processResponse(resp) {
+          // Основная логика заполнения payload
+          if (resp.hits?.hits) {
+            const hitsData = resp.hits.hits.map((hit) => ({
+              data: hit._source,
+              meta: {
+                id: hit._id,
+                index: hit._index,
+                score: hit._score,
+              },
+            }));
+
+            if (config.bulkSize) {
+              // Пакетная отправка
+              msg.payload.push(...hitsData);
+              if (msg.payload.length >= config.bulkSize) {
+                send([msg, null]);
+                msg.payload = [];
+              }
+            } else {
+              // Полная отправка
+              msg.payload = hitsData;
+            }
+          }
+
+          // Сохранение полного ответа если нужно
+          if (config.fullResponse) {
+            if (!msg.es_responses) msg.es_responses = [];
+            msg.es_responses.push(resp);
+          }
+
+          // Отправка при обычном поиске или последней партии scroll
+          if (!config.scroll || resp.hits.hits.length === 0) {
+            send([msg, null]);
+          }
+        }
+
+        function handleError(err) {
+          node.error('Search error: ' + err.message, msg);
+          msg.error = err;
+          msg.payload = [];
+          send([null, msg]);
+          if (done) done();
+        }
+      } catch (err) {
+        if (done) done(err);
+        else node.error(err, msg);
+      }
+    });
+
+    node.on('close', function (done) {
+      done();
+    });
   }
 
-  // Регистрация типа узла
-  RED.nodes.registerType("opensearch-search", opensearchSearchNode);
+  RED.nodes.registerType('opensearch-search', OpenSearchSearchNode, {
+    defaults: {
+      name: { value: '' },
+      server: { value: '' },
+      index: { value: '' },
+      query: { value: '' },
+      size: { value: 10 },
+      from: { value: 0 },
+      scroll: { value: false },
+      bulkSize: { value: 0 },
+      fullResponse: { value: false },
+    },
+  });
 };
